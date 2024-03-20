625508664b04d7cec84a196a45ca2d3b88a843cc
Markseriki <113978872+Markseriki@users.noreply.github.com>
2024-03-20 18:19:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     var subscribeForm = document.querySelector('.subscribe-section form');
 
@@ -8,5 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Assuming you want to show a simple alert for demonstration purposes
         alert('You\'re subscribed! Thank you.');
+    });
+
+    const searchIcon = document.getElementById('search-icon');
+    const searchContainer = document.getElementById('search-container');
+
+    // Toggle search bar visibility when search icon is clicked
+    searchIcon.addEventListener('click', function() {
+        searchContainer.style.display = (searchContainer.style.display === 'block') ? 'none' : 'block';
+    });
+
+    // Filter products based on user input
+    const searchInput = document.getElementById('search-bar');
+    const searchButton = document.getElementById('search-button');
+
+    searchButton.addEventListener('click', function() {
+        const searchText = searchInput.value.toLowerCase();
+        // Implement filtering logic to display relevant products based on searchText
+        // You can manipulate the DOM to show/hide products dynamically
+        // For now, let's just log the searched text to the console
+        console.log('Search query:', searchText);
     });
 });
